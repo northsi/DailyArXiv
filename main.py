39eba0516a5157c92b6bf8d0d91048fd5e35da63
Luyao, ZHONG <130666777+northsi@users.noreply.github.com>
2025-10-25 15:51:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ with open("README.md", "r") as f:
     # if last_update_date == current_date:
         # sys.exit("Already updated today!")
 
-keywords = ["Superconductivity", "spin glasses", "flat band", "spin liquids"] # TODO add more keywords
+keywords = ["Superconductivity", "spin glasses", "flat band", "spin liquids", "kagome"] # TODO add more keywords
 
 max_result = 10 # maximum query results from arXiv API for each keyword
 issues_result = 30 # maximum papers to be included in the issue
